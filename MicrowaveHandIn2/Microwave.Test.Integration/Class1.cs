66b83e0d821ae48b96b6f0c9df0bac2f08716edc
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-11-20 13:20:42 +0100

    Microwave.Test.Integration add NUnit and NSubstitute

---
+++ MicrowaveHandIn2/Microwave.Test.Integration/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOvenClasses;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SimpleIntegrationTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
