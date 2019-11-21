2ae3aee5e5e0c765fa62917d683b73df5fd1062a
Andreas Hervert Nielsen <andreas.hervert@gmail.com>
2019-11-21 14:12:25 +0100

    Test project changed

---
+++ MicrowaveHandIn2/Microwave.Test.Integration2/T7_DoorUserinterface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOvenClasses;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class T7_DoorUserinterface
+    {
+        private IDoor _door;
+        private IUserinterface _userinterface;
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _userinterface = new Userinterface();
+            _door = new Door();
+        }
+
+        [Test]
+        public void SimpleIntegrationTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
