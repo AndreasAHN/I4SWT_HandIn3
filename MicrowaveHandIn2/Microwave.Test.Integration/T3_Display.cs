5b0d5defc89444307860d539df80082efdbbd384
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-11-26 11:14:06 +0100

    Add Display integration tests

--- MicrowaveHandIn2/Microwave.Test.Integration/T3_Display.cs
+++ MicrowaveHandIn2/Microwave.Test.Integration/T3_Display.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,23 +15,46 @@ namespace Microwave.Test.Integration
     [TestFixture]
     public class T3_Display
     {
-        private IDisplay _display;
-        private Output _output;
+        private IDisplay _sut;
+        private IOutput _output;
 
+        private StringWriter _stringWriter;
 
 
         [SetUp]
         public void Setup()
         {
             _output = new Output();
-            _display = new Display(_output);
+            _sut = new Display(_output);
 
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
+        }
+
+        [TestCase(1, 3)]
+        [TestCase(0, 30)]
+        [TestCase(10, 13)]
+        public void ShowTime_ShowsCorrectTime(int min, int sec)
+        {
+            _sut.ShowTime(min, sec);
+            StringAssert.IsMatch($"Display shows: {min:D2}:{sec:D2}", _stringWriter.ToString());
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(700)]
+        [TestCase(701)]
+        public void ShowPower_ShowsCorrectPowerInWatt(int power)
+        {
+            _sut.ShowPower(power);
+            StringAssert.IsMatch($"Display shows: {power} W", _stringWriter.ToString());
         }
 
         [Test]
-        public void SimpleIntegrationTest()
+        public void Clear_ShowsDisplayCleared()
         {
-            Assert.AreEqual(1, 1);
+            _sut.Clear();
+            StringAssert.IsMatch($"Display cleared", _stringWriter.ToString());
         }
     }
 }
