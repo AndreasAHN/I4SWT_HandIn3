aa76e44cf3501083cd149d5164d57a082bb7005d
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-11-26 09:45:52 +0100

    Test console output

--- MicrowaveHandIn2/Microwave.Test.Integration/T1_Light.cs
+++ MicrowaveHandIn2/Microwave.Test.Integration/T1_Light.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -20,12 +22,20 @@ namespace Microwave.Test.Integration
         private IOutput _output;
         private ILight _sut;
 
+        // To Test Console output
+        private StringWriter _stringWriter;
+
 
         [SetUp]
         public void Setup()
         {
-            _output = Substitute.For<IOutput>();
+            _output = new Output();
             _sut = new Light(_output);
+
+            // Set Console.SetOut(_stringWriter);
+            // Console is now writing to a StringWriter instead of actual console.
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
         }
 
         [Test]
@@ -33,7 +43,7 @@ namespace Microwave.Test.Integration
         {
             _sut.TurnOn();
 
-            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains("on")));
+            StringAssert.IsMatch("Light is turned on", _stringWriter.ToString());
         }
 
         [Test]
@@ -42,7 +52,7 @@ namespace Microwave.Test.Integration
             _sut.TurnOn();
             _sut.TurnOff();
 
-            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains("off")));
+            StringAssert.IsMatch("Light is turned off", _stringWriter.ToString());
         }
 
         [Test]
@@ -50,7 +60,7 @@ namespace Microwave.Test.Integration
         {
             _sut.TurnOff();
 
-            _output.Received(0).OutputLine(Arg.Is<string>(x => x.Contains("off")));
+            StringAssert.IsMatch(string.Empty, _stringWriter.ToString());
         }
 
         [Test]
@@ -59,7 +69,7 @@ namespace Microwave.Test.Integration
             _sut.TurnOn();
             _sut.TurnOn();
 
-            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains("on")));
+            StringAssert.IsMatch("Light is turned on", _stringWriter.ToString());
         }
 
         [Test]
@@ -69,7 +79,8 @@ namespace Microwave.Test.Integration
             _sut.TurnOff();
             _sut.TurnOff();
 
-            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains("off")));
+            // Two outputs written! new line is needed.
+            StringAssert.IsMatch("Light is turned on\r\nLight is turned off", _stringWriter.ToString());
         }
     }
 }
