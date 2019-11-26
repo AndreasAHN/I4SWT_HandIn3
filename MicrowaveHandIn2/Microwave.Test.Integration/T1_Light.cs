50e79bf355d26bbd713cec05f74ba0c5fe9b9552
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-11-26 09:10:49 +0100

    Edit to bottom-up insted of top-down :)

--- MicrowaveHandIn2/Microwave.Test.Integration/T1_Light.cs
+++ MicrowaveHandIn2/Microwave.Test.Integration/T1_Light.cs
@@ -18,145 +18,48 @@ namespace Microwave.Test.Integration
     public class T1_Light
     {
         private IOutput _output;
-        private Light _uut;
+        private ILight _sut;
 
-        // Fake the user interface
-        private IUserInterface _UI;
-        private IDoor _door;
-        private IButton _startCancelButton;
-        private IButton _powerButton;
-        private IButton _timeButton;
-        private IDisplay _display;
-        private ICookController _cookController;
 
         [SetUp]
         public void Setup()
         {
             _output = Substitute.For<IOutput>();
-            _uut = new Light(_output);
-
-            _door = Substitute.For<IDoor>();
-            _startCancelButton = Substitute.For<IButton>();
-            _powerButton = Substitute.For<IButton>();
-            _timeButton = Substitute.For<IButton>();
-            _display = Substitute.For<IDisplay>();
-            _cookController = Substitute.For<ICookController>();
-
-            _UI = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _uut, _cookController);
-        }
-
-        #region OnDoorOpen
-
-        [Test]
-        public void OnDoorOpen_StateReady_LightTurnOn()
-        {
-            _door.Opened += Raise.Event();
-
-            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
+            _sut = new Light(_output);
         }
 
         [Test]
-        public void OnDoorOpen_StatePower_LightTurnOn()
+        public void TurnOn_OutputCorrect()
         {
-            _powerButton.Pressed += Raise.Event();
-            _door.Opened += Raise.Event();
+            _sut.TurnOn();
 
-            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
+            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains("on")));
         }
 
         [Test]
-        public void OnDoorOpen_StateSetTime_LightTurnOn()
+        public void TurnOff_OutputCorrect()
         {
-            _powerButton.Pressed += Raise.Event();
-            _timeButton.Pressed += Raise.Event();
-            _door.Opened += Raise.Event();
+            _sut.TurnOn();
+            _sut.TurnOff();
 
-            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
+            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains("off")));
         }
 
         [Test]
-        public void OnDoorOpen_StateCooking_LightTurnOn()
+        public void TurnOffWhileOff_OutputNothing()
         {
-            _powerButton.Pressed += Raise.Event();
-            _startCancelButton.Pressed += Raise.Event();
+            _sut.TurnOff();
 
-            _output.Received(0).OutputLine(Arg.Any<string>());
-        }
-
-
-        #endregion
-
-        #region OnDoorClose
-
-        [Test]
-        public void OnDoorClose_LightTurnOff()
-        {
-            _door.Opened += Raise.Event();
-            _door.Closed += Raise.Event();
-
-            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("off")));
-        }
-        #endregion
-
-        #region OnStartCancel
-
-        [Test]
-        public void OnStartCancel_StateReady_NothingHappens()
-        {
-            _startCancelButton.Pressed += Raise.Event();
-
-            _output.Received(0);
+            _output.Received(0).OutputLine(Arg.Is<string>(x => x.Contains("off")));
         }
 
         [Test]
-        public void OnStartCancel_StateSetPower_NothingHappens()
+        public void TurnOnWhileOn_OutputNothing2ndTime()
         {
-            _powerButton.Pressed += Raise.Event();
-            _startCancelButton.Pressed += Raise.Event();
+            _sut.TurnOn();
+            _sut.TurnOn();
 
-            _output.Received(0);
+            _output.Received(1).OutputLine(Arg.Is<string>(x => x.Contains("on")));
         }
-
-        [Test]
-        public void OnStartCancel_StateTime_LightsTurnOn()
-        {
-            _powerButton.Pressed += Raise.Event();
-            _timeButton.Pressed += Raise.Event();
-
-            _startCancelButton.Pressed += Raise.Event();
-
-            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
-        }
-
-        [Test]
-        public void OnStartCancel_StateCooking_LightsTurnOff()
-        {
-            _powerButton.Pressed += Raise.Event();
-            _timeButton.Pressed += Raise.Event();
-            _startCancelButton.Pressed += Raise.Event();
-
-            _startCancelButton.Pressed += Raise.Event();
-
-            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("off")));
-        }
-
-        #endregion
-
-        #region CookingIsDone
-
-        [Test]
-        public void CookingIsDone_LightTurnOff()
-        {
-            _powerButton.Pressed += Raise.Event();
-            _timeButton.Pressed += Raise.Event();
-            _startCancelButton.Pressed += Raise.Event();
-
-            _UI.CookingIsDone();
-
-            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("off")));
-        }
-
-        #endregion
-
     }
 }
