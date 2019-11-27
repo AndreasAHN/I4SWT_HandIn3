6a1459220bb120516ac816e87519db8c3f15e6da
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-11-27 12:27:42 +0100

    T4 not done, T5 Kind of done

--- MicrowaveHandIn2/Microwave.Test.Integration/T5_CookControllerUserInterfaceTimerLightPowerTubeDisplay.cs
+++ MicrowaveHandIn2/Microwave.Test.Integration/T5_CookControllerUserInterfaceTimerLightPowerTubeDisplay.cs
@@ -15,44 +15,144 @@ namespace Microwave.Test.Integration
     [TestFixture]
     public class T5_CookControllerUserInterfaceTimerLightPowerTubeDisplay
     {
-        private Output _output;
-        private Display _display;
-        private PowerTube _powerTube;
-        private Timer _timer;
-        private IUserInterface _fakeUserInterface;
-
-        private CookController _uut;
-
+        private IOutput _output;
+        
+        private ILight _light;
+        private IUserInterface _sut;
+        private IDoor _door;
+        private IButton _startCancelButton;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IDisplay _display;
+        private ICookController _cookController;
 
         [SetUp]
         public void Setup()
         {
-            _output = new Output();
-            _powerTube = new PowerTube(_output);
-            _display = new Display(_output);
-            _timer = new Timer();
-            _fakeUserInterface = Substitute.For<IUserInterface>();
+            _output = Substitute.For<IOutput>();
+            _light = new Light(_output);
 
-            _uut = new CookController(_timer, _display, _powerTube, _fakeUserInterface);
+            _door = Substitute.For<IDoor>();
+            _startCancelButton = Substitute.For<IButton>();
+            _powerButton = Substitute.For<IButton>();
+            _timeButton = Substitute.For<IButton>();
+            _display = Substitute.For<IDisplay>();
+            _cookController = Substitute.For<ICookController>();
+
+            _sut = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
         }
 
-        //[TestCase(1)]
-        //[TestCase(2)]
-        //[TestCase(99)]
-        //[TestCase(100)]
-        //public void StartCooking_NoExceptionThrown(int arg)
-        //{
-        //    Assert.That(() => _uut.StartCooking(arg, 60), Throws.Nothing);
-        //}
+        #region OnDoorOpen
 
-        //[TestCase(-1)]
-        //[TestCase(0)]
-        //[TestCase(101)]
-        //public void StartCooking_ExceptionThrown(int arg)
-        //{
-        //    Assert.That(() => _uut.StartCooking(arg, 60), Throws.Exception);
-        //}
+        [Test]
+        public void OnDoorOpen_StateReady_LightTurnOn()
+        {
+            _door.Opened += Raise.Event();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
+        }
+
+        [Test]
+        public void OnDoorOpen_StatePower_LightTurnOn()
+        {
+            _powerButton.Pressed += Raise.Event();
+            _door.Opened += Raise.Event();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
+        }
+
+        [Test]
+        public void OnDoorOpen_StateSetTime_LightTurnOn()
+        {
+            _powerButton.Pressed += Raise.Event();
+            _timeButton.Pressed += Raise.Event();
+            _door.Opened += Raise.Event();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
+        }
+
+        [Test]
+        public void OnDoorOpen_StateCooking_LightTurnOn()
+        {
+            _powerButton.Pressed += Raise.Event();
+            _startCancelButton.Pressed += Raise.Event();
+
+            _output.Received(0).OutputLine(Arg.Any<string>());
+        }
 
 
+        #endregion
+
+        #region OnDoorClose
+
+        [Test]
+        public void OnDoorClose_LightTurnOff()
+        {
+            _door.Opened += Raise.Event();
+            _door.Closed += Raise.Event();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("off")));
+        }
+        #endregion
+
+        #region OnStartCancel
+
+        [Test]
+        public void OnStartCancel_StateReady_NothingHappens()
+        {
+            _startCancelButton.Pressed += Raise.Event();
+
+            _output.Received(0);
+        }
+
+        [Test]
+        public void OnStartCancel_StateSetPower_NothingHappens()
+        {
+            _powerButton.Pressed += Raise.Event();
+            _startCancelButton.Pressed += Raise.Event();
+
+            _output.Received(0);
+        }
+
+        [Test]
+        public void OnStartCancel_StateTime_LightsTurnOn()
+        {
+            _powerButton.Pressed += Raise.Event();
+            _timeButton.Pressed += Raise.Event();
+
+            _startCancelButton.Pressed += Raise.Event();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
+        }
+
+        [Test]
+        public void OnStartCancel_StateCooking_LightsTurnOff()
+        {
+            _powerButton.Pressed += Raise.Event();
+            _timeButton.Pressed += Raise.Event();
+            _startCancelButton.Pressed += Raise.Event();
+
+            _startCancelButton.Pressed += Raise.Event();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("off")));
+        }
+
+        #endregion
+
+        #region CookingIsDone
+
+        [Test]
+        public void CookingIsDone_LightTurnOff()
+        {
+            _powerButton.Pressed += Raise.Event();
+            _timeButton.Pressed += Raise.Event();
+            _startCancelButton.Pressed += Raise.Event();
+
+            _sut.CookingIsDone();
+
+            _output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("off")));
+        }
+
+        #endregion
     }
 }
