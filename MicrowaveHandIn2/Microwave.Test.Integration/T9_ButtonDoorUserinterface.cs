5f1d988fe1fd74ac36480bc7973970db825cdb01
Andreas Hervert Nielsen <andreas.hervert@gmail.com>
2019-11-27 13:57:24 +0100

    T9 opsat og påbegyndt

--- MicrowaveHandIn2/Microwave.Test.Integration/T9_ButtonDoorUserinterface.cs
+++ MicrowaveHandIn2/Microwave.Test.Integration/T9_ButtonDoorUserinterface.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,30 +7,57 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using NSubstitute;
 using MicrowaveOvenClasses;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
 
 namespace Microwave.Test.Integration
 {
-    //[TestFixture]
-    //public class T9_ButtonDoorUserinterface
-    //{
-    //    private IDoor _door;
-    //    private IButton _button;
-    //    private IUserinterface _userinterface;
+    [TestFixture]
+    public class T9_ButtonDoorUserinterface
+    {
+        private IButton _startCancelButton;
+        private IButton _powerButton;
+        private IButton _timeButton;
+
+        private IDoor _door;
+        private IOutput _output;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private ILight _light;
+        private ITimer _timer;
+        private ICookController _cookController;
+        private IUserInterface _userinterface;
+        
+        private StringWriter _stringWriter;
 
 
+        [SetUp]
+        public void Setup()
+        {
+            _startCancelButton = new Button();
+            _powerButton = new Button();
+            _timeButton = new Button();
 
-    //    [SetUp]
-    //    public void Setup()
-    //    {
-    //        _userinterface = new Userinterface();
-    //        _door = new Door();
-    //        _button = new Button();
-    //    }
+            _door = new Door();
+            _output = new Output();
+            _display = new Display(_output);
+            _powerTube = new PowerTube(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _userinterface = new UserInterface(_powerButton, _timeButton, startCancelButton:, _door, _display, _light, _cookController);
 
-    //    [Test]
-    //    public void SimpleIntegrationTest()
-    //    {
-    //        Assert.AreEqual(1, 1);
-    //    }
-    //}
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
+        }
+
+        [Test]
+        public void SimpleIntegrationTest()
+        {
+            _cookController.UI = _userinterface;
+
+            _powerButton.Press();
+        }
+    }
 }
