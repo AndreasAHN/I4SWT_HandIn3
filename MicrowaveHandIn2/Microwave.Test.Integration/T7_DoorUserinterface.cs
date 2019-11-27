ccd2e55e86ba620d14481be1eb1c49743c918b91
Malou Troelsen <au543213@uni.au.dk>
2019-11-27 18:54:03 +0100

    T7 + T8 done

--- MicrowaveHandIn2/Microwave.Test.Integration/T7_DoorUserinterface.cs
+++ MicrowaveHandIn2/Microwave.Test.Integration/T7_DoorUserinterface.cs
@@ -5,29 +5,107 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using NSubstitute;
-using MicrowaveOvenClasses;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
 
 namespace Microwave.Test.Integration
 {
-    //[TestFixture]
-    //public class T7_DoorUserinterface
-    //{
-    //    private IDoor _door;
-    //    private IUserinterface _userinterface;
+    [TestFixture]
+    public class T7_DoorUserinterface
+    {
+        private IDoor _sut;
+        private IUserInterface _userInterface;
+        private CookController _cookController;
+        private IDisplay _display;
+        private ILight _light;
+        private IButton _powerButton, _timeButton, _startCancelButton; // fake
+        private ITimer _timer;           // fake
+        private IPowerTube _powerTube;   // fake
+        private IOutput _output;         // fake
 
 
 
-    //    [SetUp]
-    //    public void Setup()
-    //    {
-    //        _userinterface = new Userinterface();
-    //        _door = new Door();
-    //    }
+            [SetUp]
+        public void Setup()
+        {
+            _sut = new Door(); 
+            _powerButton = Substitute.For<IButton>();
+            _timeButton = Substitute.For<IButton>();
+            _startCancelButton = Substitute.For<IButton>();
+            _timer = Substitute.For<ITimer>();
+            _powerTube = Substitute.For<IPowerTube>();
+            _output = Substitute.For<IOutput>();
 
-    //    [Test]
-    //    public void SimpleIntegrationTest()
-    //    {
-    //        Assert.AreEqual(1, 1);
-    //    }
-    //}
+            _light = new Light(_output);
+            _display = new Display(_output);
+            
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _sut, _display, _light, _cookController);
+            _cookController.UI = _userInterface;
+        }
+
+        [Test]
+        public void DoorOpenedWhileIdle()
+        {
+            _userInterface.OnDoorOpened(this, EventArgs.Empty);
+
+            _output.Received(1).OutputLine("Light is turned on");
+        }
+
+        [Test]
+        public void DoorOpenedThenClosedWhileIdle()
+        {
+            _userInterface.OnDoorOpened(this, EventArgs.Empty);
+            _userInterface.OnDoorClosed(this, EventArgs.Empty);
+
+            Received.InOrder(() =>
+            {
+                _output.OutputLine("Light is turned on");
+                _output.OutputLine("Light is turned off");
+                _output.OutputLine("Display cleared");
+            });
+
+        }
+
+        [Test]
+        public void DoorOpenedDuringSetup()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _userInterface.OnDoorOpened(this, EventArgs.Empty);
+
+            _output.Received(1).OutputLine("Light is turned on");
+        }
+
+        [Test]
+        public void DoorOpenedThenClosedDuringSetup()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _userInterface.OnDoorOpened(this, EventArgs.Empty);
+            _userInterface.OnDoorClosed(this, EventArgs.Empty);
+
+            Received.InOrder(() =>
+            {
+                _output.OutputLine("Light is turned on");
+                _output.OutputLine("Light is turned off");
+            });
+        }
+
+        [Test]
+        public void DoorOpenedWhileRunning()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+            _userInterface.OnDoorOpened(this, EventArgs.Empty);
+
+            _output.Received(1).OutputLine("Light is turned on");
+
+
+        }
+
+
+    }
 }
--- MicrowaveHandIn2/Microwave.Test.Integration/T8_ButtonUserinterface.cs
+++ MicrowaveHandIn2/Microwave.Test.Integration/T8_ButtonUserinterface.cs
@@ -1,33 +1,206 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using NSubstitute;
-using MicrowaveOvenClasses;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
 
 namespace Microwave.Test.Integration
 {
-    //[TestFixture]
-    //public class T8_ButtonUserinterface
-    //{
-    //    private IButton _button;
-    //    private IUserinterface _userinterface;
+    [TestFixture]
+    public class T8_ButtonUserinterface
+    {
+        private IButton _powerButton, _timeButton, _startCancelButton; // sut
+        private IUserInterface _userInterface;
+        private CookController _cookController;
+        private IDisplay _display;
+        private ILight _light;
+        private IDoor _door; // fake
+        private ITimer _timer; // fake 
+        private IPowerTube _powerTube; // fake
+        private IOutput _output; // fake
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _door = Substitute.For<IDoor>();  //new Door();
+            _timer = Substitute.For<ITimer>(); // new Timer();
+            _output = Substitute.For<IOutput>();
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _powerTube = new PowerTube(_output);
 
 
 
-    //    [SetUp]
-    //    public void Setup()
-    //    {
-    //        userinterface = new Userinterface();
-    //        _button = new Button();
-    //    }
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light,
+                _cookController);
+            _cookController.UI = _userInterface;
+        }
 
-    //    [Test]
-    //    public void SimpleIntegrationTest()
-    //    {
-    //        Assert.AreEqual(1, 1);
-    //    }
-    //}
+        [Test]
+        public void PowerButtonPressedOnce_Shows50W()
+        {
+            _userInterface.OnPowerPressed(this,EventArgs.Empty);
+
+            _output.Received(1).OutputLine("Display shows: 50 W");
+        }
+
+        [Test]
+        public void PowerButtonPressedTwice_Shows100W()
+        {
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+
+            _output.Received(1).OutputLine("Display shows: 100 W");
+        }
+
+        [TestCase(1, 50)]
+        [TestCase(2, 100)]
+        [TestCase(10, 500)]
+        [TestCase(14, 700)]
+        public void PowerButtonPressedNtimes(int nPresses, int expected)
+        {
+            for (int i = 0; i < nPresses; i++)
+            {
+                _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            }
+            _output.Received(1).OutputLine($"Display shows: {expected} W");
+        }
+
+        [Test]
+        public void TimeButtonPressedOnce_Shows0100()
+        {
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            _userInterface.OnTimePressed(this, EventArgs.Empty);
+            
+            _output.Received(1).OutputLine("Display shows: 01:00");
+        }
+
+        [Test]
+        public void TimeButtonPressedTwice_Shows0200()
+        {
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            _userInterface.OnTimePressed(this, EventArgs.Empty);
+            _userInterface.OnTimePressed(this, EventArgs.Empty);
+
+            _output.Received(1).OutputLine("Display shows: 02:00");
+        }
+
+        [TestCase(1, "01:00")]
+        [TestCase(2, "02:00")]
+        [TestCase(6, "06:00")]
+        [TestCase(11, "11:00")]
+        public void TimeButtonPressedNtimes(int nPresses, string expected)
+        {
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            for (int i = 0; i < nPresses; i++)
+            {
+                _userInterface.OnTimePressed(this, EventArgs.Empty);
+            }
+            _output.Received(1).OutputLine($"Display shows: {expected}");
+        }
+
+        [Test]
+        public void StartCancelBtnPressedAfterPowerBtn()
+        {
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+
+            Received.InOrder(() =>
+                {
+                    _output.OutputLine("Display shows: 50 W");
+                    _output.OutputLine("Display cleared");
+                });
+        }
+
+        [Test]
+        public void StartCancelBtnPressedAfterPowerBtnTwice()
+        {
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            _userInterface.OnPowerPressed(this,EventArgs.Empty);
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+            Received.InOrder(() =>
+            {
+                _output.OutputLine("Display shows: 50 W");
+                _output.OutputLine("Display shows: 100 W");
+                _output.OutputLine("Display cleared");
+            });
+        }
+
+        [Test]
+        public void StartCancelBtnPressedTwiceAfterPowerBtn()
+        {
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+
+            Received.InOrder(() =>
+            {
+                _output.OutputLine("Display shows: 50 W");
+                _output.OutputLine("Display cleared");
+            });
+        }
+
+        [Test]
+        public void StartCancelBtnPressedAfterPowerAndTimeBtn()
+        {
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            _userInterface.OnTimePressed(this, EventArgs.Empty);
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+            Received.InOrder(() =>
+            {
+                _output.OutputLine("Display shows: 50 W");
+                _output.OutputLine("Display shows: 01:00");
+                _output.OutputLine("Display cleared");
+                _output.OutputLine("Light is turned on");
+            });
+        }
+
+        [Test]
+        public void StartCancelBtnPressedTwiceAfterPowerAndTimeBtnTwice()
+        {
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            _userInterface.OnPowerPressed(this, EventArgs.Empty);
+            _userInterface.OnTimePressed(this, EventArgs.Empty);
+            _userInterface.OnTimePressed(this, EventArgs.Empty);
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+            Received.InOrder(() =>
+            {
+                _output.OutputLine("Display shows: 50 W");
+                _output.OutputLine("Display shows: 100 W");
+                _output.OutputLine("Display shows: 01:00");
+                _output.OutputLine("Display shows: 02:00");
+                _output.OutputLine("Display cleared");
+                _output.OutputLine("Light is turned on");
+                _output.OutputLine("Light is turned off");
+                _output.OutputLine("Display cleared");
+            });
+        }
+
+        [Test]
+        public void StartCancelBtnPressedFirstOnce_NothingHappens()
+        {
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+
+            _output.Received(0);
+        }
+
+        [Test]
+        public void StartCancelBtnPressedFirstTwice_NothingHappens()
+        {
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+            _userInterface.OnStartCancelPressed(this, EventArgs.Empty);
+
+            _output.Received(0);
+        }
+
+
+
+    }
 }
